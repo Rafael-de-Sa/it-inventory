8a358850aa490b4eb900eb4a6df867b1b386bd12
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-07-30 15:08:35 -0300

    :sparkles: feat: login form validation - email and password required (AuthController validation)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,28 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo 'Login Submit';
+        //form validation
+        $request->validate(
+            [
+                'email' => ['required', 'email'],
+                'password' => ['required']
+            ],
+            //error messages
+            [
+                'email.required' => 'O e-mail é obrigatório',
+                'email.email' => 'O e-mail deve ser válido',
+                'password.required' => 'A senha é obrigatória'
+            ]
+        );
+
+        //get user input
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+
+        echo $email;
+        echo '<br>';
+        echo $password;
     }
 
     public function logout()
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,10 +4,10 @@
     <div class="w-full flex justify-center bg-green-950 py-7">
 
         <form action="{{ route('loginSubmit') }}" method="post"
-            class="w-3/5 bg-green-900 p-8 rounded-xl shadow-md text-white font-inter">
+            class="w-3/5 bg-green-900 p-8 rounded-xl shadow-md text-white font-inter" novalidate>
 
             @csrf
-            <div class="space-y-12">
+            <div class="space-y-8">
 
                 <div class="pb-6 border-b border-green-600">
                     <h2 class="text-lg font-semibold">Login</h2>
@@ -18,18 +18,35 @@
 
                 <div class="pb-6 border-b border-green-600">
 
-                    <div class="mt-10 grid grid-cols-1 gap-6 sm:grid-cols-6">
+                    <div class="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-6">
 
                         <div class="sm:col-span-6">
                             <label for="email" class="block text-sm font-medium text-white">Email</label>
-                            <input id="email" type="email" name="email" autocomplete="email"
+                            <input id="email" type="email" name="email" autocomplete="email" required
+                                value="{{ old('email') }}"
                                 class="mt-2 block w-full rounded-md bg-green-950 border border-green-600 px-4 py-2 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 transition duration-300" />
+
+                            @error('email')
+                                <div class="col-span-full bg-green-950 border-l-4 border-red-500 text-red-300 px-4 py-2 rounded-md mt-6 text-sm"
+                                    role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
+
+                        <!--TODO: Colocar required no formulário -->
 
                         <div class="col-span-full">
                             <label for="password" class="block text-sm font-medium text-white">Senha</label>
-                            <input id="password" type="password" name="password" autocomplete="current-password"
+                            <input id="password" type="password" name="password" autocomplete="current-password" required
                                 class="mt-2 block w-full rounded-md bg-green-950 border border-green-600 px-4 py-2 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 transition duration-300" />
+
+                            @error('password')
+                                <div class="col-span-full bg-green-950 border-l-4 border-red-500 text-red-300 px-4 py-2 rounded-md mt-6 text-sm"
+                                    role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
 
                     </div>

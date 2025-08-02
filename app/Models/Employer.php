1212b8c47c8d9ca010286dfb832e705bd47e3a27
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-08-02 13:11:56 -0300

    :sparkles: feat: create employer model and migration

---
+++ app/Models/Employer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Employer extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    // Campos que podem ser preenchidos via create/update
+    protected $fillable = [
+        'trade_name',
+        'corporate_name',
+        'cnpj',
+        'street',
+        'number',
+        'complement',
+        'neighborhood',
+        'city',
+        'state',
+        'zipcode',
+        'website',
+        'email',
+        'active',
+        'phones',
+        'logo_path'
+    ];
+
+    // Casts para tipos específicos
+    protected $casts = [
+        'phones' => 'array',
+        'active' => 'boolean'
+    ];
+}
